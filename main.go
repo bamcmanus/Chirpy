324f8b80c299cf78a9eda6c3e29dc9d48b28b625
bamcmanus <brent.mcmanus@me.com>
2025-03-22 21:58:25 -0700

    add validate chirp functionality

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
+
+type request struct {
+    Body string `json:"body"`
+}
+
 
 type apiConfig struct {
     fileserverHits atomic.Int32
@@ -18,24 +25,90 @@ func (c *apiConfig) middlewareMetricsInt(next http.Handler) http.Handler {
     })
 }
 
+func cleanseWords(body string) string {
+    profaneWords := map[string]struct{}{ "kerfuffle": {}, "sharbert": {}, "fornax": {}}
+    lowerBody := strings.ToLower(body)
+    words := strings.Split(body, " ")
+    lowerWords := strings.Split(lowerBody, " ")
+
+    for i, word := range lowerWords {
+        if _, ok := profaneWords[word]; ok {
+            words[i] = "****"
+        }
+    }
+
+    return strings.Join(words, " ")
+}
+
 func main() {
     var cfg apiConfig
     mux := http.NewServeMux()
 
-    mux.Handle("/app/", cfg.middlewareMetricsInt(http.FileServer(http.Dir("."))))
+    mux.Handle("/app/", cfg.middlewareMetricsInt(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
-    mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, req *http.Request) {
+    mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
         w.Header().Add("Content-Type", "text/plain; charset=utf-8")
         w.WriteHeader(http.StatusOK)
         fmt.Fprintf(w, "OK")
     })
 
-    mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, req *http.Request) {
-        w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-        fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+    mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, req *http.Request) {
+        type errResponse struct {
+            Error string `json:"error"`
+        }
+        
+        type cleanedResponse struct {
+            Body string `json:"cleaned_body"`
+        }
+
+        decoder := json.NewDecoder(req.Body)
+        var params request
+        if err := decoder.Decode(&params); err != nil {
+            log.Printf("error decoding prameters: %s", err)
+            w.WriteHeader(http.StatusInternalServerError)
+            resp := errResponse{Error: "Something went wrong"}
+            jsonData, err := json.Marshal(resp)
+            if err != nil {
+                log.Fatal("could not marshal response")
+            }
+            w.Write(jsonData)
+            return
+        }
+
+        if len(params.Body) > 140 {
+            resp := errResponse{Error: "Chirp is too long"}
+            w.WriteHeader(http.StatusBadRequest)
+            jsonData, err := json.Marshal(resp)
+            if err != nil {
+                log.Fatal("could not marshal response")
+            }
+            w.Write(jsonData)
+            return
+        }
+
+        body := cleanseWords(params.Body)
+
+        resp := cleanedResponse{Body: body}
+        jsonData, err := json.Marshal(resp)
+        if err != nil {
+            log.Fatal("could not marshal response")
+        }
+        w.Write(jsonData)
     })
 
-    mux.HandleFunc("POST /reset", func(w http.ResponseWriter, req *http.Request) {
+    mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, req *http.Request) {
+        w.Header().Add("Content-Type", "text/html")
+        template := `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+        htmlContent := fmt.Sprintf(template, cfg.fileserverHits.Load())
+        fmt.Fprint(w, htmlContent)
+    })
+
+    mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, req *http.Request) {
         cfg.fileserverHits.Store(0)
         w.Header().Add("Content-Type", "text/plain; charset=utf-8")
         w.WriteHeader(http.StatusOK)
@@ -49,6 +122,6 @@ func main() {
 
     log.Println("Starting server ...")
     if err := server.ListenAndServe(); err != nil {
-        log.Fatal("failed to start server")
+        log.Fatalf("failed to start server: %s", err)
     }
 }
