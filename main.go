ee542f59f3dda1c244924d9f04df854c55e07190
bamcmanus <brent.mcmanus@me.com>
2025-03-23 14:35:05 -0700

    add sql/user functionality

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/bamcmanus/Chirpy/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type request struct {
@@ -16,6 +25,7 @@ type request struct {
 
 type apiConfig struct {
     fileserverHits atomic.Int32
+    platform string
 }
 
 func (c *apiConfig) middlewareMetricsInt(next http.Handler) http.Handler {
@@ -41,8 +51,65 @@ func cleanseWords(body string) string {
 }
 
 func main() {
+    godotenv.Load()
+    dbUrl := os.Getenv("DB_URL")
+    db, err := sql.Open("postgres", dbUrl)
+    if err != nil {
+        log.Fatalf("failed to connect to database; err: %s", err)
+    }
+
+    dbQueries := database.New(db)
+    
     var cfg apiConfig
+    platform := os.Getenv("PLATFORM")
+    cfg.platform = platform
     mux := http.NewServeMux()
+
+    mux.HandleFunc("POST /api/users", func(w http.ResponseWriter, req *http.Request) {
+        type newUserRequest struct {
+            Email string `json:"email"`
+        }
+
+        type newUserResponse struct {
+            Id uuid.UUID `json:"id"`
+            CreatedAt time.Time `json:"created_at"`
+            UpdatedAt time.Time `json:"updated_at"`
+            Email string `json:"email"`
+        }
+
+        var newUserReq newUserRequest
+        decoder := json.NewDecoder(req.Body)
+        if err := decoder.Decode(&newUserReq); err != nil {
+            log.Println("could not decode new user request")
+            w.WriteHeader(http.StatusInternalServerError)
+            return
+        }
+
+        user, err := dbQueries.CreateUser(context.Background(), newUserReq.Email)
+        if err != nil {
+            log.Printf("failed to create user; err: %s", err)
+            w.WriteHeader(http.StatusInternalServerError)
+            return
+        }
+
+        res := newUserResponse {
+            Email: user.Email,
+            CreatedAt: user.CreatedAt,
+            UpdatedAt: user.UpdatedAt,
+            Id: user.ID,
+        }
+
+        jsonData, err := json.Marshal(res)
+        if err != nil {
+            log.Printf("failed to marshal created user; err: %s", err)
+            w.WriteHeader(http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusCreated)
+        w.Write(jsonData)
+    })
 
     mux.Handle("/app/", cfg.middlewareMetricsInt(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
@@ -109,6 +176,17 @@ func main() {
     })
 
     mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, req *http.Request) {
+        if cfg.platform != "dev" {
+            w.WriteHeader(http.StatusForbidden)
+            return
+        }
+
+        if err := dbQueries.DeleteUsers(context.Background()); err != nil {
+            log.Printf("error deleting users; err: %s", err)
+            w.WriteHeader(http.StatusInternalServerError)
+            return
+        }
+
         cfg.fileserverHits.Store(0)
         w.Header().Add("Content-Type", "text/plain; charset=utf-8")
         w.WriteHeader(http.StatusOK)
